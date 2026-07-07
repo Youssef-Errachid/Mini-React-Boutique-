2797eb800a3ded67018b135f37acfde211ad8269
Youssef <yousseferrachid@gmail.com>
2026-07-07 18:09:18 +0100

    add routting for each page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-import "../../styles/Header.css";
+import { NavLink } from "react-router-dom";
 import { AiFillShop } from "react-icons/ai";
 import { FaCartArrowDown } from "react-icons/fa6";
+import "../../styles/Header.css";
 
 const Header = ({ cartCount, onCartClick }) => {
   return (
     <header className="header">
       <div className="navbar">
-        <div className="navbar-logo">
+        <NavLink to="/" className="navbar-logo">
           <AiFillShop size={30} />
           <span>Boutique</span>
-        </div>
+        </NavLink>
 
-        <div className="cart-icon" onClick={onCartClick}>
-          <FaCartArrowDown size={22} />
-          <span>Cart</span>
-          <span className="cart-count">{cartCount}</span>
+        <div className="navbar-actions">
+          <NavLink
+            to="/add-product"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            + Add Product
+          </NavLink>
+
+          <div className="cart-icon" onClick={onCartClick}>
+            <FaCartArrowDown size={22} />
+            <span>Cart</span>
+            <span className="cart-count">{cartCount}</span>
+          </div>
         </div>
       </div>
     </header>

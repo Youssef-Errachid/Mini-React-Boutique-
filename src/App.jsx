8eda0091b92ad0393d4f20d07a69fb3c33f8722a
Youssef <yousseferrachid@gmail.com>
2026-07-07 11:57:52 +0100

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import productsData from "./data/products.json";
-
 import Header from "./components/Header/Header";
-import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
-import ProductList from "./components/ProductList/ProductList";
 import Cart from "./components/Cart/Cart";
 import Footer from "./components/Footer/Footer";
+import AppRouter from "./AppRouter";
 
 const App = () => {
   const [products, setProducts] = useState(productsData);
@@ -15,46 +13,36 @@ const App = () => {
 
   const categories = ["All", ...new Set(products.map((p) => p.category))];
 
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
-  const removeFromCart = (productId) => {
+  const addToCart = (product) => setCart([...cart, product]);
+  const removeFromCart = (productId) =>
     setCart(cart.filter((item) => item.id !== productId));
-  };
-
-  const removeFromCatalog = (productId) => {
+  const removeFromCatalog = (productId) =>
     setProducts(products.filter((p) => p.id !== productId));
-  };
+  const addProduct = (newProduct) => setProducts([...products, newProduct]);
 
   return (
     <div>
       <Header cartCount={cart.length} onCartClick={() => setShowCart(true)} />
 
-      <CategoryFilter
-        categories={categories}
-        selectedCategory={selectedCategory}
-        onSelectCategory={setSelectedCategory}
-      />
-
-      <ProductList
+      <AppRouter
         products={products}
         selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        categories={categories}
         onAddToCart={addToCart}
         onRemoveFromCatalog={removeFromCatalog}
+        onAddProduct={addProduct}
       />
 
       {showCart && (
         <div className="overlay" onClick={() => setShowCart(false)} />
       )}
-
       <Cart
         cart={cart}
         removeFromCart={removeFromCart}
         showCart={showCart}
         setShowCart={setShowCart}
       />
-
       <Footer />
     </div>
   );

615b04fbe24752c9c11785ec2bf3e24b87b49473
Youssef <yousseferrachid@gmail.com>
2026-07-08 10:54:44 +0100

    adding the pagee home page and the page of 404 page not found and the styles of 404

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import CategoryFilter from "../components/CategoryFilter/CategoryFilter";
+import ProductList from "../components/ProductList/ProductList";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const HomePage = ({
+  products,
+  selectedCategory,
+  setSelectedCategory,
+  categories,
+  onAddToCart,
+  onRemoveFromCatalog,
+}) => {
+  return (
+    <div>
+      <CategoryFilter
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+      <ProductList
+        products={products}
+        selectedCategory={selectedCategory}
+        onAddToCart={onAddToCart}
+        onRemoveFromCatalog={onRemoveFromCatalog}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/NotFoundPage.css";
+import { FaGhost } from "react-icons/fa";
 
 const NotFoundPage = () => {
-  return <div>404 - Page Not Found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="not-found">
+      <h1 className="not-found-code">
+        {""}
+        <FaGhost size="80px" /> 404
+      </h1>
+      <p className="not-found-message">Page not found.</p>
+      <button className="not-found-btn" onClick={() => navigate("/")}>
+        Back to Home
+      </button>
+    </div>
+  );
 };
 
 export default NotFoundPage;
